24246dfea53c6c5bde214155118c2f2bf24aa45c
deepakbarr <deepak.barr@flipkart.com>
2016-03-09 15:53:02 +0530

    Update Constants.java

--- order-status/src/main/java/com/hackathon/Constants.java
+++ order-status/src/main/java/com/hackathon/Constants.java
@@ -4,7 +4,7 @@ package com.hackathon;
  * Created by deepak.barr on 05/06/15.
  */
 public class Constants {
-    public final static String password = "lionel10";
+    public final static String password = "";
     public final static String AMAZON = "AMAZON";
     public final static String FLIPKART = "FLIPKART";
     public final static String DISPATCHED="DISPATCHED";
